defdbedea15abd28201c82a0a105385ca1489012
kaho-kitagawa <ho.ka1.26.kita@gmail.com>
2021-09-04 02:47:55 +0900

    スクリプトの宿題

---
+++ ScriptLearning/Assets/Question1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question1 : MonoBehaviour
+{
+    public int Number;
+    void Start()
+    {
+        if (Number % 2 == 0)
+        {
+            Debug.Log("偶数");
+        }
+        else
+        {
+            Debug.Log("奇数");
+        }
+    }
+
+}
---
+++ ScriptLearning/Assets/Question2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question2 : MonoBehaviour
+{
+    public int Year;
+    void Start()
+    {
+        if (Year % 400 == 0)
+        {
+            Debug.Log("うるう年だよ");
+        }
+        else if (Year % 100 == 0)
+        {
+            Debug.Log("うるう年じゃないよ");
+        }
+        else if (Year % 4 == 0)
+        {
+            Debug.Log("うるう年だよ");
+        }
+        else
+        {
+            Debug.Log("うるう年じゃないよ");
+        }
+    }
+}
